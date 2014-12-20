66859e4158249cb3e3155b9899d721e6d92314f7
Uncle Gigi <unclegigi@outlook.com>
2014-12-20 18:01:00 +0100

    Add Server.js

---
+++ Server.js
@@ -0,0 +1,8 @@
+var express = require('express').express();
+
+
+var server = express.listen(3000, function89 {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Server listening at http://%s:%s', host, port)
+});
